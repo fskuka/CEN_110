c1af3411fd5401a4763cb622157091d342deccd2
fskuka <skuka.f@gmail.com>
2017-03-31 16:39:08 +0200

    CEN_110
    Lab Solutions

---
+++ Lab/Week_3/Number Of Letters/Number Of Letters/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+	int lowCh = 0, uppCh = 0,i=0;
+	char ch, text[251];
+
+	FILE *infile;
+	infile = fopen("input.txt", "r");
+	if (infile == NULL)
+		exit(1);
+	do
+	{
+		ch = getc(infile);
+		text[i] = ch;
+		if (islower(ch)) lowCh++;
+		if (isupper(ch)) uppCh++;
+
+		++i;
+	} while (ch != EOF);
+	
+
+	printf("%d %d", uppCh, lowCh);
+
+	fclose(infile);
+	return 0;
+}
+
